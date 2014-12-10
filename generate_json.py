a24d5df66ba846fd9733e15de2fd1c009f0bc1c3
Sanghee Kim <sangheestyle@gmail.com>
2014-12-09 23:55:06 -0700

    show group

--- generate_json.py
+++ generate_json.py
@@ -8,20 +8,25 @@ git_dir_path = sys.argv[1]
 output_file = sys.argv[2]
 
 r = Repo()
-r.read_repo(git_dir_path, 5)
+r.read_repo(git_dir_path, 2)
 g = nx.Graph()
 for commit in r:
     for change in commit['change']:
-        g.add_node(commit['author_email'], bipartite=0)
-        g.add_node(change['filename'], bipartite=1)
-        g.add_edge(commit['author_email'], change['filename'])
+        if change['del'] > 0 and change['ins'] > 0:
+            company = commit['author_email'].split('@')[-1]
+            g.add_node(commit['author_email'],
+                       bipartite=0,
+                       company=company)
+            g.add_node(change['filename'], bipartite=1)
+            g.add_edge(commit['author_email'], change['filename'])
 top_nodes = set(n for n, d in g.nodes(data=True) if d['bipartite'] == 0)
 bottom_nodes = set(g) - top_nodes
 dev_graph = nx.bipartite.projected_graph(g, top_nodes)
 remove = [node for node, degree
           in dev_graph.degree().items()
-          if degree == 0]
+          if degree < 1]
 dev_graph.remove_nodes_from(remove)
+print "Number of vertices:", len(dev_graph)
 d3_formatted = json_graph.node_link_data(dev_graph)
 with open(output_file, 'wb') as f:
     json.dump(d3_formatted, f, indent=4)
