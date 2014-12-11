4a307c2443d65ae2cd26f56fe7a6e8bee2045a5f
Sanghee Kim <sangheestyle@gmail.com>
2014-12-11 15:52:49 -0700

    RFT: make developer graph implementation seperately

---
+++ developer_graph.py
@@ -0,0 +1,61 @@
+import sys
+import json
+
+import networkx as nx
+from networkx.readwrite import json_graph
+
+
+class DeveloperGraph(object):
+    """
+    Read repository and convert it into a graph
+    """
+
+    def __init__(self):
+        self._g = nx.Graph()
+        self._projected_graph = None
+
+    @property
+    def graph(self):
+        return self._g
+
+    @property
+    def projected_graph(self):
+        return self._projected_graph
+
+    def read_repo(self, repo):
+        for commit in repo:
+            self._build_graph(commit)
+        top_nodes = set(n for n, d in self._g.nodes(data=True) if d['bipartite'] == 0)
+        bottom_nodes = set(self._g) - top_nodes
+        self._projected_graph = nx.bipartite.projected_graph(self._g, top_nodes)
+        remove = [node for node, degree
+                  in self._projected_graph.degree().items()
+                  if degree < 1]
+        self._projected_graph.remove_nodes_from(remove)
+
+    def _build_graph(self, commit):
+        author_email = commit['author_email']
+        company = author_email.split('@')[-1]
+        self._g.add_node(author_email, bipartite=0, company=company)
+        for change in commit['change']:
+            if change['filename'].endswith('.java'):
+                self._g.add_node(change['filename'], bipartite=1)
+                self._g.add_edge(commit['author_email'], change['filename'])
+                insertions = 0
+                deletions = 0
+                if type(change['del']) is int:
+                    deletions = change['del']
+                if type(change['ins']) is int:
+                    insertions = change['ins']
+                if ("deletions" not in self._g.node[author_email]) or \
+                   ("insertions" not in self._g.node[author_email]):
+                    self._g.node[author_email].update({"deletions":deletions})
+                    self._g.node[author_email].update({"insertions":insertions})
+                else:
+                    self._g.node[author_email]["deletions"] += deletions
+                    self._g.node[author_email]["insertions"] += insertions
+
+    def write_json(self, output_path):
+        d3_formatted = json_graph.node_link_data(self._projected_graph)
+        with open(output_path, 'wb') as f:
+            json.dump(d3_formatted, f, indent=4)
--- generate_json.py
+++ generate_json.py
@@ -1,11 +1,8 @@
 import sys
-import json
 import argparse
 
-import networkx as nx
-from networkx.readwrite import json_graph
-
 from repo import Repo
+from developer_graph import DeveloperGraph
 
 
 parser = argparse.ArgumentParser(description='Generate node link data to be used by presenter')
@@ -19,36 +16,7 @@ output_file = args.o
 
 r = Repo()
 r.read_repo(git_dir_path, args.m)
-g = nx.Graph()
-for commit in r:
-    author_email = commit['author_email']
-    company = author_email.split('@')[-1]
-    g.add_node(author_email, bipartite=0, company=company)
-    for change in commit['change']:
-        if change['del'] > 0 and change['ins'] > 0:
-            g.add_node(change['filename'], bipartite=1)
-            g.add_edge(commit['author_email'], change['filename'])
-            insertions = 0
-            deletions = 0
-            if type(change['del']) is int:
-                deletions = change['del']
-            if type(change['ins']) is int:
-                insertions = change['ins']
-            if ("deletions" not in g.node[author_email]) or \
-               ("insertions" not in g.node[author_email]):
-                g.node[author_email].update({"deletions":deletions})
-                g.node[author_email].update({"insertions":insertions})
-            else:
-                g.node[author_email]["deletions"] += deletions
-                g.node[author_email]["insertions"] += insertions
-top_nodes = set(n for n, d in g.nodes(data=True) if d['bipartite'] == 0)
-bottom_nodes = set(g) - top_nodes
-dev_graph = nx.bipartite.projected_graph(g, top_nodes)
-remove = [node for node, degree
-          in dev_graph.degree().items()
-          if degree < 1]
-dev_graph.remove_nodes_from(remove)
-print "Number of vertices:", len(dev_graph)
-d3_formatted = json_graph.node_link_data(dev_graph)
-with open(output_file, 'wb') as f:
-    json.dump(d3_formatted, f, indent=4)
+dev_graph = DeveloperGraph()
+dev_graph.read_repo(r)
+print "Number of vertices:", len(dev_graph.projected_graph)
+dev_graph.write_json(args.o)
